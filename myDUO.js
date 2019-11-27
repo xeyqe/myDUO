935e45d6d2cb44966b01b4a3b4bc87efb1f2f30c
xeyqe <xeyqe@seznam.cz>
2019-11-27 20:45:37 +0100

    hint sentence

--- myDUO.js
+++ myDUO.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Duolingo Improver
-// @version      2.8.9.4
+// @version      2.8.9.5
 // @description  For description visit https://github.com/xeyqe/myDUO/blob/master/README.md
 // @icon         https://res.cloudinary.com/dn6n8yqqh/image/upload/c_scale,h_214/v1555635245/Icon_qqbnzf.png
 // @author       xeyqe
@@ -450,7 +450,10 @@ function neco(color) {
                             .children.length-1].innerText;
 
         if (document.querySelector('[data-test="hint-sentence"]') != null) {
-            question = document.querySelector('[data-test="hint-sentence"]').innerText;
+            const listOfTokens = document.querySelectorAll('[data-test="hint-token"]');
+            for (let i of listOfTokens) {
+                question += i.innerText.split("\n")[0];
+            }
             var spanList = document.querySelectorAll('label span');
             if (spanList.length) {
                 var append = "";
