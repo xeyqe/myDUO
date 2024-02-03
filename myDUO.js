9e2fc682e6e9e9eaaadb889df99e9d81d37c0bbe
xeyqe <xeyqe@seznam.cz>
2024-02-03 23:20:35 +0100

    tapComplete fill answer to question

--- myDUO.js
+++ myDUO.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Duolingo Improver
-// @version      3.0.4.3
+// @version      3.0.4.4
 // @description  For description visit https://github.com/xeyqe/myDUO/blob/master/README.md
 // @icon         https://res.cloudinary.com/dn6n8yqqh/image/upload/c_scale,h_214/v1555635245/Icon_qqbnzf.png
 // @author       xeyqe
@@ -640,7 +640,9 @@ function neco(color) {
             question = document.querySelector('[role="radiogroup"]').previousElementSibling.textContent;
         } else if (document.querySelector('[data-test="challenge challenge-tapComplete"]')) {
             question = Array.from(document.querySelectorAll('[aria-hidden="true"]')).map(el => el.textContent).join('').replace(/\s\s/g, ' ___ ');
-            yourAnswer = Array.from(document.querySelector('[aria-hidden="true"]').parentElement.parentElement.parentElement.querySelectorAll('[data-test="challenge-tap-token-text"]')).map(it => it.innerText).join().replace(/[,]+/g, ', ')
+            const el = document.querySelector('[aria-hidden="true"]').parentElement.parentElement.parentElement;
+            const ar = Array.from(el.querySelectorAll('[data-test="challenge-tap-token-text"]')).map(it => it.innerText).filter(i => i);
+            ar.forEach(it => question = question.replace(' ___ ', ` _${it}_ `));
         } else if (document.querySelector('[data-test="challenge challenge-dialogue"]')) {
             footerHidden = localStorage.getItem('footerHidden') === "true"
             if (footerHidden) hideShowFooter(true);
